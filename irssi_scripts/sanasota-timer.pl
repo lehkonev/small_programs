d505d4257e099f3d805a316364b5354dfd4ae538
Veini Lehkonen <veini.lehkonen@student.tut.fi>
2017-11-05 22:18:56 +0200

    Two other timerscripts I found laying around. At least one of these if not both is modeled after a timer script I found somewhere else.

---
+++ irssi_scripts/sanasota-timer.pl
@@ -0,0 +1,163 @@
+#
+# This is a simple special-purpose timer for word wars.
+
+use strict;
+use Data::Dumper;
+use warnings;
+#use vars  qw ($VERSION %IRSSI);
+use Irssi #20020325 qw (command_bind command_runsub command timeout_add timeout_remove signal_add_first);
+
+our $VERSION = '1.00';
+our %IRSSI = (
+    authors     => 'Veini Lehkonen' ,
+    contact     => 'lehkonev@gmail.com' ,
+    name        => 'Sanasotailija',
+    description => 'Works mostly like a timer, but is specified for word wars in specific channels.' ,
+    license     => 'Public Domain',
+    changed     => '2015-10-08'
+    );
+
+our %timers;
+# my %timer = { repeat => \d+, command => '' , windowitem => NULL , server=> NULL, timer = NULL};
+
+sub timer_command {
+    my ( $name ) = @_;
+    if ( exists ( $timers{$name} ) ) {
+        if ( $timers{$name}->{'repeat'} != -1 ) {
+            if ( $timers{$name}->{'repeat'}-- == 0) {
+                cmd_timerstop( $name );
+                return;
+            }
+        }
+
+        my ($server, $item);
+        if ($timers{$name}->{'server'}) {
+            $server = Irssi::server_find_tag( $timers{$name}->{'server'} );
+        }
+        if ( $server ) {
+	    if ( $timers{$name}->{'windowitem'}) {
+                $item = $server->window_find_item( $timers{$name}->{'windowitem'} );
+            }
+            ($item ? $item : $server)->command( $timers{$name}->{'command'} );
+        } else {
+            command( $timers{$name}->{'command'} );
+        }
+    }
+}
+
+sub cmd_timerstop {
+    my ( $name ) = @_;
+
+    if ( exists ( $timers{$name} ) ) {
+        timeout_remove($timers{$name}->{'timer'});
+        $timers{$name} = ();
+        delete ( $timers{$name} );
+        print( CRAP "Timer \"$name\" stopped." );
+    }
+    else {
+        print( CRAP "\cBTimer:\cB No such timer \"$name\"." );
+    }
+}
+
+sub cmd_timer_help {
+    print ( <<EOF
+
+TIMER LIST
+TIMER ADD  <name> <interval in seconds> [<repeat>] <command>
+TIMER STOP <name>
+
+repeat value of 0 means unlimited too
+
+EOF
+	);
+}
+
+command_bind 'timer add' => sub {
+    my ( $data, $server, $item ) = @_;
+    my ( $name, $interval, $times, $command );
+
+    if ( $data =~ /^\s*(\w+)\s+(\d+(?:\.\d+)?)\s+(-?\d+)\s+(.*)$/ ) {
+        ( $name, $interval, $times, $command ) = ( $1, $2, $3, $4 );
+        $times = -1 if ( $times == 0 );
+    }
+    elsif ( $data =~ /^\s*(\w+)\s+(\d+(?:\.\d+)?)\s+(.*)$/ )
+    {
+        ( $name, $interval, $times, $command ) = ( $1, $2, -1, $3 );
+    }
+    else {
+        print( CRAP "\cBTimer:\cB parameters not understood. commandline was: timer add $data");
+        return;
+    };
+
+    if ( $times < -1 ) {
+        print( CRAP "\cBTimer:\cB repeat should be greater or equal to -1" );
+        return;
+    };
+
+    if ( $command eq "" ) {
+        print( CRAP "\cBTimer:\cB command is empty commandline was: timer add $data" );
+        return;
+    };
+
+    if ( exists ( $timers{$name} ) ) {
+        print( CRAP "\cBTimer:\cB Timer \"$name\" already active." );
+    }
+    else {
+        #$timers{$name} = {};
+        $timers{$name}->{'repeat'}     = $times;
+        $timers{$name}->{'interval'}   = $interval;
+        $timers{$name}->{'command'}    = $command;
+	if ($item) {
+            $timers{$name}->{'windowitem'} = $item->{'name'};
+	}
+	if ($server) {
+            $timers{$name}->{'server'}     = $server->{'tag'};
+	}
+
+        if ( $times == -1 ) {
+            $times = 'until stopped.';
+        }
+        else {
+            $times .= " times.";
+        }
+
+        print( CRAP "Starting timer \"$name\" repeating \"$command\" every $interval seconds $times" );
+
+        $timers{$name}->{'timer'} = timeout_add( $interval * 1000, \&timer_command, $name );
+    }
+};
+
+command_bind 'timer list' => sub {
+    print( CRAP "Active timers:" );
+    foreach my $name ( keys %timers ) {
+        if ( $timers{$name}->{repeat} == -1 ) {
+            print( CRAP "$name = $timers{$name}->{'command'} (until stopped)");
+        }
+        else {
+            print( CRAP "$name = $timers{$name}->{'command'} ($timers{$name}->{'repeat'} repeats left)" );
+        }
+    }
+    print( CRAP "End of /timer list" );
+};
+
+command_bind 'timer stop' => sub {
+    my ( $data, $server, $item ) = @_;
+    cmd_timerstop ($data);
+};
+
+command_bind 'timer help' => sub { cmd_timer_help() };
+
+command_bind 'timer' => sub {
+    my ( $data, $server, $item ) = @_;
+    $data =~ s/\s+$//g;
+    command_runsub ( 'timer', $data, $server, $item ) ;
+};
+
+
+signal_add_first 'default command timer' => sub {
+#
+# gets triggered if called with unknown subcommand
+#
+    cmd_timer_help()
+}
+
---
+++ irssi_scripts/sanasota.pl
@@ -0,0 +1,111 @@
+# Script for word wars (called sanasota in Finnish) (can be edited to be any kind of simple timer).
+# This scripts works in irssi after it came out of beta, but there are other issues with it.
+
+use strict;
+use warnings;
+use Irssi;
+
+use vars qw($VERSION %IRSSI);
+$VERSION = "0.4";
+%IRSSI = (
+  authors     => 'Veini Lehkonen',
+  contact     => 'lehkonev@gmail.com',
+  name        => 'sanasota',
+  description => 'Simple timer that can be used in specific channels.',
+  license     => 'Public Domain',
+  url         => '',
+  changed     => '2015-11-17'
+);
+
+use Irssi::Irc;
+
+# The command will only be accepted from these channels.
+our @kanavat = ("#nanowrimo", "#finnnano");
+# This is the command that will trigger the timer.
+our $sanasota = "!sanasota";
+# This is the default time (minutes).
+our $perusaika = 10;
+# This determines how many timers can be active at one time.
+our $montako_sanasotaa_max = 5;
+our $montako_sanasotaa = 0;
+our @palvelimet;
+our @viestit;
+
+sub lopeta_sanasota
+{
+
+        # Testiprintti, toivottavasti omaan status-ikkunaan.
+        #print ( CRAP "Eka on $_[0], toka on $_[1]." );
+
+        #my ($palvelin, $viesti) = split('¤', $_[0], 2);
+        #print ( CRAP "Palvelin on $palvelin ja viesti on $viesti." );
+        #my Irssi::Server $pal = $palvelin;
+        #print ( CRAP "Globaali palvelin on $palv." );
+        #$pal->command("$viesti sanasota loppui!");
+        my $i = $_[0];
+        $palvelimet[$i]->command("$viestit[$i] sanasota loppui!");
+        $montako_sanasotaa--;
+}
+
+sub kampaa_sanasota
+{
+        my ($palvelin, $viesti, $nick, $osoite, $kanava) = @_;
+        my ($kasky, $aika, $yksikko) = split(' ', $viesti, 3);
+        my $vastaus = "msg " . $kanava . " " . $nick . ": ";
+        my $aikayksikko = 1;
+
+        # If the word was the correct command defined in $sanasota
+        # and the channel is inside @kanavat
+        # and the time is a number...
+        if ( ($kasky ~~ $sanasota) and
+             ($kanava ~~ @kanavat) )
+        {
+                # If there are too many timers, new one won’t be made.
+                if ( $montako_sanasotaa > $montako_sanasotaa_max )
+                {
+                        $palvelin->command( $vastaus . "Ei pysty, liian monta sanasotaa käynnissä.");
+                }
+                else
+                {
+                        if ( !defined $aika )
+                        {
+                                $aika = 10;
+                        }
+
+                        if ( #(Scalar::Util::looks_like_number($aika)) and
+                             ($aika < 120.1) and
+                             ($aika > 0.1) )
+                        {
+                                $vastaus .= $aika . ":n ";
+
+                                # Let’s check if the caller wanted seconds. If not, assume minutes.
+                                if ($yksikko ~~ "s")
+                                {
+                                        $vastaus .= "sekunnin";
+                                }
+                                else
+                                {
+                                        $aikayksikko = 60;
+                                        $vastaus .= "minuutin";
+                                }
+
+                                $palvelin->command("$vastaus sanasota alkaa nyt!");
+                                #print ( CRAP "Palvelin on $palvelin ja vastaus on $vastaus." );
+# Here is a stupid concatenation because arrays did not work for me.
+                                $montako_sanasotaa++;
+                                #my $parametri = $palvelin . "¤" . $vastaus;
+                                #$palv = $palvelin;
+                                $palvelimet[$montako_sanasotaa] = $palvelin;
+                                $viestit[$montako_sanasotaa] = $vastaus;
+                                my $tagi = Irssi::timeout_add_once($aikayksikko * $aika * 1000, \&lopeta_sanasota, ($montako_sanasotaa));
+                                #Irssi::timeout_remove($tagi);
+                        }
+                        else
+                        {
+                                $palvelin->command($vastaus . "Ei pysty, annettu aika on kummallinen.");
+                        }
+                }
+        }
+}
+
+Irssi::signal_add ("message public", "kampaa_sanasota");
